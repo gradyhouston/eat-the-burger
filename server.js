756709f8b0d42caeb46fe0ed88cdf9fc6377f993
Grady <Gradyhouston@gmail.com>
2018-05-22 10:11:20 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3306;
+var port = process.env.PORT || 3306;
 
 var db = require("./models");
 
